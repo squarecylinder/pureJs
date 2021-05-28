3341732bff67a1733e2a610570c05bcd9f091dc3
Square Cylinder <ziencck@gmail.com>
2021-05-28 13:55:28 -0400

    adding ui, speed, different directions, smoother paddle

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
-let canvas = document.getElementById("gameScreen");
-let ctx = canvas.getContext("2d");
-
+let ctx = document.getElementById("gameScreen").getContext('2d')
 
 //Canvas width and height
-const GW = 800;
-const GH = 600;
+const GW = 600;
+const GH = 450;
+// state of key press
 let rightPressed = false;
 let leftPressed = false;
 
+let score = 0;
+// Defines the angle of the balls starting pos
+let strBallYDir = Math.ceil(Math.random() * 4);
+// Defines if ball points left or right to begin
+let strBallXDir = Math.ceil(Math.random() * 5) * (Math.round(Math.random()) ? 1 : -1);
+console.log(strBallXDir)
 // Ball class
 class Ball {
     // Size of the ball and how it
     constructor(){
         this.ballX = GW / 2;
         this.ballY = GH - 50;
-        this.radius = 10;
+        this.radius = 8;
         this.delta = {
-            dx: 2,
-            dy: -2
+            dx: strBallXDir,
+            dy: -strBallYDir
         }
     }
     // How we draw a ball shape
@@ -32,8 +37,6 @@ class Ball {
         // Changes direction of ball
         this.ballX += this.delta.dx 
         this.ballY += this.delta.dy
-        // Deletes the last position of the ball off the game screen
-        ctx.clearRect(0, 0, GW, GH);
         // Checks if the ball hits the sides of the game screen
         if(this.ballX + this.delta.dx > GW - this.radius || this.ballX + this.delta.dx < this.radius){
             this.delta.dx = -this.delta.dx;
@@ -41,6 +44,20 @@ class Ball {
         // Checks to see if the ball hits the top of the game screen
         if(this.ballY + this.delta.dy < this.radius){
             this.delta.dy = -this.delta.dy;
+        }
+        // Ball & Paddle Collision
+        if(this.ballY + this.delta.dy < this.radius || 
+            (
+            this.ballY + this.delta.dy > GH - paddle.height - this.radius &&
+            this.ballX + this.delta.dx > paddle.position.x &&
+            this.ballX + this.delta.dx < paddle.position.x + paddle.width
+            ) 
+        ){
+            score++
+            this.delta.dy = -this.delta.dy
+        }
+        else if(this.ballY + this.delta.dy > GH){
+            location.reload()
         }
     }
 }
@@ -56,25 +73,24 @@ class Paddle {
     }
     // Drawing a paddle
     draw(ctx){
+        ctx.beginPath();
+        ctx.rect(this.position.x, this.position.y, this.width, this.height)
         ctx.fillStyle = '#7fcab4';
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+        ctx.fill()
+        ctx.closePath()
     }
     // Updates left or right based on input
     update(lp, rp){
-    ctx.clearRect(0, 0, 800, 600)
     if(rp && (this.position.x + this.width) < GW ){
-        console.log(rp);
-        this.position.x += 20;
+        this.position.x += 10;
         rp = false
-        console.log(rp)
         }
     if(lp && this.position.x > 0){
-        this.position.x += -20;
+        this.position.x += -10;
         lp = false
         }
     }
 }
-
 // Instation
 let ball = new Ball()
 let paddle = new Paddle(GW, GH);
@@ -82,38 +98,44 @@ let paddle = new Paddle(GW, GH);
 // Checks input for movement
 const startMovement = (e) => {
     if(e.code === 'KeyA' || e.code === 'ArrowLeft'){
-        leftPressed = true;
-        paddle.update(leftPressed, false)
+        leftPressed = true
         }
     if(e.code === 'KeyD' || e.code === 'ArrowRight'){
         rightPressed = true
-        paddle.update(false, rightPressed)
         }
     }
 
     const stopMovement = (e) => {
     if(e.code === 'KeyA' || e.code === 'ArrowLeft'){
-        console.log('test')
         leftPressed = false
-        paddle.update(leftPressed, false)
         }
     if(e.code === 'KeyD' || e.code === 'ArrowRight'){
         rightPressed = false
-        paddle.update(false, rightPressed)
         }
 }
-
+const uiScore = () => {
+    ctx.font = '18px sans-serif';
+    ctx.fillText(`Score: ${score}`, 0, 15);
+}
+const uiSpeed = () => {
+    ctx.font = '18px serif';
+    ctx.fillText(`Speed: ${strBallYDir}`, 100, 15)
+}
 // The loop
-const gameLoop = () => { 
+const gameLoop = () => {
     document.addEventListener("keydown", startMovement);
     document.addEventListener("keyup", stopMovement);
+    ctx.clearRect(0, 0, GW, GH)
+    uiScore(ctx)
+    uiSpeed(ctx)
     // changes the ball movement
     ball.update(ctx);
     ball.draw(ctx);
     paddle.draw(ctx);
-
+    paddle.update(leftPressed, rightPressed)
+// Calls the loop again
     requestAnimationFrame(gameLoop);
 }
-
+setTimeout(gameLoop, 1000 / 60)
 // Requests the loop every frame
 requestAnimationFrame(gameLoop);
